4b43ce085781650c347b971c801282dfaef0f2a0
Angelo Rafael <angelorpt@gmail.com>
2021-02-21 18:48:49 -0400

    add | Validação dos campos

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -5,6 +5,24 @@ class ClienteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cliente
         fields = '__all__'
+
     def validate_cpf(self, cpf):
         if len(cpf) != 11:
             raise serializers.ValidationError("O CPF deve possuir 11 dígitos")
+        return cpf
+
+    def validate_nome(self, nome):
+        if not nome.alpha():
+            raise serializers.ValidationError("Não inclua números neste campo")
+        return nome
+
+    def validate_rg(self, rg):
+        if len(rg) != 9:
+            raise serializers.ValidationError("O RG deve possuir 9 dígitos")
+        return rg
+    
+    def validate_celular(self, celular):
+        if len(celular) < 11:
+            raise serializers.ValidationError("O celular deve possuir 11 dígitos")
+        return celular
+    
