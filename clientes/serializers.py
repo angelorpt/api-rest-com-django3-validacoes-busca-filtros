5a1c404718342955f900f2389a0a1e10a00b747f
Angelo Rafael <angelorpt@gmail.com>
2021-02-21 19:07:08 -0400

    fix | condições de valiação do Serializers.py

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -11,13 +11,13 @@ class ClienteSerializer(serializers.ModelSerializer):
         if not cpf_valido(data['cpf']):
             raise serializers.ValidationError({'cpf': "O CPF deve ter 11 dígitos"})
 
-        if not nome_valido(data['nome'])
+        if not nome_valido(data['nome']):
             raise serializers.ValidationError({'nome': "Não inclua números neste campo"})
 
         if not rg_valido(data['rg']):
             raise serializers.ValidationError({'rg': "O RG deve possuir 9 dígitos"})
 
-        if not celular_valido(data['celular'])
+        if not celular_valido(data['celular']):
             raise serializers.ValidationError({'celular': "O celular deve possuir 11 dígitos"})
         
         return data
